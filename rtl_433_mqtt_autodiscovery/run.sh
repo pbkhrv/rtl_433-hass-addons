98c15f390eb3aac6d25b761571e0cf2dabcc28a5
Peter Bakhirev <pbkhrv@pm.me>
2020-12-26 20:55:12 -0500

    Fix discovery_topic key name

--- rtl_433_mqtt_autodiscovery/run.sh
+++ rtl_433_mqtt_autodiscovery/run.sh
@@ -5,8 +5,8 @@ MQTT_PORT=$(bashio::config "mqtt_port")
 MQTT_USER=$(bashio::config "mqtt_user")
 MQTT_PASS=$(bashio::config "mqtt_password")
 RTL_TOPIC=$(bashio::config "rtl_topic")
-DISCOVERY_TOPIC_PREFIX=$(bashio::config "discovery_topic_prefix")
+DISCOVERY_TOPIC=$(bashio::config "discovery_topic")
 DISCOVERY_INTERVAL=$(bashio::config "discovery_interval")
 
 echo "Starting rtl_433_mqtt_hass.py..."
-python3 /rtl_433_mqtt_hass.py -d -u $MQTT_USER -P $MQTT_PASS -H $MQTT_HOST -p $MQTT_PORT -R "$RTL_TOPIC" -D "$DISCOVERY_TOPIC_PREFIX" -i $DISCOVERY_INTERVAL
+python3 /rtl_433_mqtt_hass.py -d -u $MQTT_USER -P $MQTT_PASS -H $MQTT_HOST -p $MQTT_PORT -R "$RTL_TOPIC" -D "$DISCOVERY_TOPIC" -i $DISCOVERY_INTERVAL
